b5aa3cadf31378a503d3b07b05066929bb67d023
Ravinder verma <ravinderverma@Ravinders-MacBook-Air.local>
2024-08-08 19:37:47 +0530

    File Modified

--- 7_Aug/home/urls.py
+++ 7_Aug/home/urls.py
@@ -1,8 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from home.views import ProductDetailAPIView
+# from home.views import ProductDetailAPIView
+from home.views import ProductAPI
+
 
 urlpatterns = [
-    path('product/', ProductDetailAPIView.as_view())
+    path('product/', ProductAPI.as_view()),
+
+
 ]
--- 7_Aug/home/views.py
+++ 7_Aug/home/views.py
@@ -1,5 +1,6 @@
-from rest_framework import generics
+from rest_framework import generics, permissions, authentication
 from rest_framework.decorators import APIView
+from rest_framework.response import Response
 from home.models import Product
 from home.serializers import ProductSerializer
 
@@ -11,6 +12,21 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     # lookup_field = 'pk'
 
 
-
 class ProductAPI(APIView):
-    pass
+    # session authentication and permission
+
+    permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    authentication_classes = [authentication.SessionAuthentication]
+    
+    def get(self, request):
+        objs = Product.objects.filter()
+
+        serializer = ProductSerializer(objs, many=True)
+
+        return Response({
+            'status':True,
+            'data':serializer.data,
+            'message':'product fetch successfully'
+        })
+    
